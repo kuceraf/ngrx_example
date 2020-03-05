f9be8263147d726eeacb045d7be7a23e63da4610
Filip Kucera <kucerafilip@gmail.com>
2020-03-05 18:42:31 +0100

    entity

--- src/app/modules/user-feature/store/user.reducer.ts
+++ src/app/modules/user-feature/store/user.reducer.ts
@@ -1,12 +1,20 @@
 import {UserActionTypes} from "./user.actions";
 import {UserState} from "./user-state";
 import {UserFeature} from "../user-feature";
+import {createEntityAdapter, EntityAdapter, EntityState} from "@ngrx/entity";
+import {User} from "../user";
+import {createFeatureSelector} from "@ngrx/store";
 
 const initialState: UserFeature = {
   loading: false,
   users: [],
   error: void 0
 };
+
+export interface State extends EntityState<User>{}
+export const adapter: EntityAdapter<User> = createEntityAdapter();
+export const initialStateA: State = adapter.getInitialState();
+// NOTE - use adapter to replace repeating code like this: adapter.removeOne(action.payload, state);
 
 export const userReducer = (state: UserFeature = initialState, action) => {
   switch (action.type) {
@@ -26,3 +34,8 @@ export const userReducer = (state: UserFeature = initialState, action) => {
   }
 };
 
+// export const userSelector = createFeatureSelector('userFeature');
+export const {
+  selectAll,
+  selectTotal
+} = adapter.getSelectors();
